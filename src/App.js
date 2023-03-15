88bf4a551cdb346a3e2250215aa093913a392f4f
clever-cr <cleverumuhuza@gmail.com>
2023-03-15 15:58:26 +0200

    Add list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const animals = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-40'>
+      {animals.map((animal, index) => {
+        return (<li key={index}>{animal}</li>)
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
