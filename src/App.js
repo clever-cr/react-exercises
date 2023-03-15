76e7b1ceed0b39bacc66a6edbe327ed4cd74d531
clever-cr <cleverumuhuza@gmail.com>
2023-03-15 12:02:36 +0200

    add alert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
+  const handleClick = () => {
+    alert(" Clicked!")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='px-32 py-12 space-y-12'>
+      <div className='space-x-5'>
+        {["Home", "Exercise1", "Exercise2", "Exercise3", "Exercise4", "Exercise5", "Exercise6", "Exercise7", "Exercise8", "Exercise9"].map((link, index) => {
+          return (
+
+            <a key={index} href={index} className='text-blue-900 underline'>{link}</a>
+          )
+        })}
+      </div>
+      <button onClick={handleClick} className='bg-blue-900 p-2 ml-32 text-white'>Click me </button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
