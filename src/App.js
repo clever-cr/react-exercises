360e99cf08ce52141c0f1282d2c17508d139c0b5
clever-cr <cleverumuhuza@gmail.com>
2023-03-16 15:05:53 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import page1 from './page1'
+const App = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: ""
+  })
+  const handleChange = (event) => {
 
-function App() {
+    setData(prevData => {
+      return {
+        ...prevData,
+        [event.target.name]: event.target.value
+      }
+    })
+
+  }
+  const handleClick = (event) => {
+    event.preventDefault()
+    alert(`Hello ${data.firstName} ${data.lastName} `)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-32 '>
+      <form className='flex flex-col w-48'>
+
+        <input type="text" placeholder='First Name' name="firstName" value={data.firstName} onChange={handleChange} />
+        <input t ype="text" name="lastName" placeholder='First Name' value={data.lastName} onChange={handleChange} />
+
+      </form>
+      <button className='bg-[#F0003C]' onClick={handleClick}>Greet me </button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
