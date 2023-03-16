0a0987d8f946535b2cff70cd359973f61fd5230a
clever-cr <cleverumuhuza@gmail.com>
2023-03-16 15:33:01 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import page1 from './page1'
 const App = () => {
   const [data, setData] = useState({
     firstName: "",
@@ -20,14 +19,14 @@ const App = () => {
     alert(`Hello ${data.firstName} ${data.lastName} `)
   }
   return (
-    <div className='p-32 '>
-      <form className='flex flex-col w-48'>
+    <div className='p-32 space-y-5 w-[500px] bg-gray-50 m-12'>
+      <form className='flex flex-col w-48- gap-2 w-full'>
 
-        <input type="text" placeholder='First Name' name="firstName" value={data.firstName} onChange={handleChange} />
-        <input t ype="text" name="lastName" placeholder='First Name' value={data.lastName} onChange={handleChange} />
+        <input type="text" placeholder='First Name' name="firstName" value={data.firstName} onChange={handleChange} className="border-2 p-1 rounded-md border-gray-300 placeholder:text-black" />
+        <input type="text" name="lastName" placeholder='last Name' value={data.lastName} onChange={handleChange} className="border-2 p-1 rounded-md border-gray-300 placeholder:text-black" />
 
       </form>
-      <button className='bg-[#F0003C]' onClick={handleClick}>Greet me </button>
+      <button className='bg-[#F0003C] p-3 rounded-md text-lg text-white w-full' onClick={handleClick}>Greet me </button>
 
     </div>
   )
