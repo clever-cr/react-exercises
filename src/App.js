04a29d35a829e9b3816e1b1a45e6ca3798b7e5df
clever-cr <cleverumuhuza@gmail.com>
2023-03-17 16:02:48 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ const App = () => {
 
   return (
     <div className='bg-blue-50 flex p-12 gap-12 h-screen' >
-      {data.map((info, index) => {
-        return (<Card key={index} setup={info.setup} punchline={info.punchline} />)
-
+      {data.map(info => {
+        return (<Card key={info.id} setup={info.setup} punchline={info.punchline} />)
       })}
     </div>
   )
