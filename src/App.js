73fdb35ff052dc87fb1bf226766f585bcffe127a
clever-cr <cleverumuhuza@gmail.com>
2023-03-20 15:22:14 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import { useState, useEffect } from 'react'
 import Card from "./components/Card"
+
 const App = () => {
   const nav = ["Home", "Exercise1", "Exercise2", "Exercise3", "Exercise4", "Exercise5", "Exercise6", "Exercise7", "Exercise8", "Exercise9"]
   const [userData, setUserData] = useState([]);
-  const get = async () => {
+
+  const getUsers = async () => {
     const res = await fetch('https://random-data-api.com/api/users/random_user?size=10')
     const data = await res.json()
     setUserData(data)
   }
+
   useEffect(() => {
-    get();
+    getUsers();
   }, [])
 
   return (
     <div>
       <div className='space-x-5 mx-40 pt-4'>
-        {nav.map((link, index) => {
-          return (
-            <a key={index} href={index} className='text-blue-900 underline'>{link}</a>
-          )
-        })}
+        {nav.map((link, index) => <a key={index} href={index} className='text-blue-900 underline'>{link}</a>)}
       </div>
       <div className='px-32 py-12 space-y-12'>
-        <button className='bg-red-600 text-white rounded-md mx-52 p-2 shadow-md' onClick={get}>Fetch Random</button>
-        <Card data={userData} />
+        <button className='bg-red-600 text-white rounded-md mx-52 p-2 shadow-md' onClick={getUsers}>Fetch Random</button>
+        <div className='grid grid-cols-2 bg-green-50 gap-12 p-12'>
+          {userData.map((item) => {
+            return (
+              <Card
+                key={item.id}
+                avatar={item.avatar}
+                first_name={item.first_name}
+                last_name={item.last_name}
+                employment={item.employment.title}
+              />)
+          })}
+        </div>
       </div >
     </div>
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,22 @@
 import "../App.css"
-const card = ({ data }) => {
+const Card = ({ avatar, first_name, last_name, employment }) => {
     return (
-        <div className='grid grid-cols-2 bg-green-50 gap-12 p-12'>
-            {data.map(user => (
-                <div key={user.id} className="bg-white flex flex-col p-4 rounded-md w-[500px] flip-card ">
-                    <div className='flip-card-inner'>
-                        <div className='flip-card-front space-y-3'>
-                            <img src={user.avatar} alt="avatar" />
-                            <h1 className="font-bold">{user.first_name} {user.last_name}</h1>
-                            <h1 className='ktext-lg'>{user.employment.title}</h1>
-                        </div>
-                        <div className="flip-card-back">
-                            <h1 className='text-xl font-bold top-0'>More information here</h1>
-                        </div>
-
+        <div>
+            <div className="bg-white flex flex-col p-4 rounded-md w-[500px] flip-card ">
+                <div className='flip-card-inner'>
+                    <div className='flip-card-front space-y-3'>
+                        <img src={avatar} alt="avatar" />
+                        <h1 className="font-bold">{first_name} {last_name}</h1>
+                        <h1 className='ktext-lg'>{employment}</h1>
                     </div>
-
+                    <div className="flip-card-back">
+                        <h1 className='text-xl font-bold top-0'>More information here</h1>
+                    </div>
                 </div>
-            ))
-            }
+
+            </div>
         </div >
     )
 }
 
-export default card
+export default Card
