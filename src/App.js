cd547badb9153a5e21cec61f1f782e12a0d4f67e
clever-cr <cleverumuhuza@gmail.com>
2023-03-20 11:18:45 +0200

    add an array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import Card from "./components/Card"
 const App = () => {
+  const nav = ["Home", "Exercise1", "Exercise2", "Exercise3", "Exercise4", "Exercise5", "Exercise6", "Exercise7", "Exercise8", "Exercise9"]
   const [userData, setUserData] = useState([]);
   const get = async () => {
     const res = await fetch('https://random-data-api.com/api/users/random_user?size=10')
